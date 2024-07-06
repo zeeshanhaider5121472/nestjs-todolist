85a2ca28e6fe61220fe06cc98ffaac0749577be4
zeeshanhaider5121472 <shani721287@gmail.com>
2024-07-07 02:31:04 +0500

    testing3

--- api.ts
+++ api.ts
@@ -1,10 +1,16 @@
 import { TableTasks } from "./types/tabledata";
 
-const baseurl = "http://localhost:3001";
+// const baseurl = "http://localhost:3001";
 
 // // Fetch data from API
+// export const gettabledata = async () => {
+
+// };
+
+// Fetch data from API
 export const gettabledata = async (): Promise<TableTasks[]> => {
-  const res = await fetch(`${baseurl}/tasks`, { cache: "no-store" });
+  // const res = await fetch(`${baseurl}/tasks`, { cache: "no-store" });
+  const res = await fetch("http://localhost:3001/tasks", { cache: "no-store" });
   const data = res.json();
   return data;
 };
